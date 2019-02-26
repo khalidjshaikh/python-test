5663feb7260f6d322f3ecdbcac7ad6aa9808e4d7
Khalid Shaikh <k@ruby.run>
2019-02-25 18:01:19 -0800

    init commit

---
+++ test.py
@@ -0,0 +1,23 @@
+'''
+:type magic_number: int 
+:type numbers: list[int]
+:type rtype: bool
+'''
+
+def add(sum, numbers):
+    print(numbers, sum)
+    if(numbers == []):
+        return(sum)
+    return(add(sum + numbers[0], numbers[1:]))
+
+def arithmetic_boggle(magic_number, numbers):
+    sum = add(0, numbers)
+    print(sum, magic_number);
+    return(sum == magic_number)
+
+print(arithmetic_boggle(6, [1,2,3]));
+
+print(arithmetic_boggle(0, []));
+print(arithmetic_boggle(43, []));
+print(arithmetic_boggle(42, [42]));
+print(arithmetic_boggle(0, [99]));
